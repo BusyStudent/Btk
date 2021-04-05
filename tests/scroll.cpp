9baad02afdc89e8a0a152c8e97a90c5f962f73f6
liuli-neko <2982886437@qq.com>
2021-04-05 10:58:39 +0800

    测试

--- tests/scroll.cpp
+++ tests/scroll.cpp
@@ -3,7 +3,8 @@
 int main(){
     Btk::Init();
     Btk::Window win("ScrollBar test",600,600);
-    auto &bar = win.add<Btk::ScrollBar>(Btk::Orientation::H);
-    bar.set_rect({0,0,600,20});
+    auto &bar = win.add<Btk::ScrollBar>(Btk::Orientation::V);
+    auto &bar1 = win.add<Btk::ScrollBar>(Btk::Orientation::H);
+    // bar.set_rect({0,0,600,10});
     win.mainloop();
 }
